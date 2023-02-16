4f0a69e69a71243eec1886c6fd0881ee02ce3f53
Andor Molnar <andor@cloudera.com>
2023-02-16 15:38:24 +0100

    Add command line parser

--- src/main/java/SnapshotExpiry.java
+++ src/main/java/SnapshotExpiry.java
@@ -1,40 +1,92 @@
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.jute.BinaryInputArchive;
+import org.apache.jute.InputArchive;
+import org.apache.zookeeper.server.DataTree;
+import org.apache.zookeeper.server.persistence.FileSnap;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.Adler32;
 import java.util.zip.CheckedInputStream;
 
-public class SnapshotExpirer {
+public class SnapshotExpiry implements AutoCloseable {
+
+  private final String snapshotFileName;
+
+  public SnapshotExpiry(String snapshotFile) {
+    this.snapshotFileName = snapshotFile;
+  }
 
   /**
-   * USAGE: SnapshotFormatter snapshot_file
+   * USAGE: SnapshotExpiry snapshot_file
    */
   public static void main(String[] args) throws Exception {
-    if (args.length != 1) {
-      System.err.println("USAGE: SnapshotFormatter snapshot_file");
-      System.exit(2);
-    }
+    try (final SnapshotExpiry se = parseCommandLine(args)) {
+      assert se != null;
+      se.run();
 
-    new SnapshotExpirer().run(args[0]);
+
+      System.exit(0);
+    }
   }
 
-  public void run(String snapshotFileName) throws IOException {
+  public void run() throws IOException {
     InputStream is = new CheckedInputStream(
-        new BufferedInputStream(new FileInputStream(snapshotFileName)),
+        new BufferedInputStream(Files.newInputStream(Paths.get(snapshotFileName))),
         new Adler32());
     InputArchive ia = BinaryInputArchive.getArchive(is);
-
     FileSnap fileSnap = new FileSnap(null);
-
     DataTree dataTree = new DataTree();
     Map<Long, Integer> sessions = new HashMap<Long, Integer>();
-
     fileSnap.deserialize(dataTree, sessions, ia);
-
-    printDetails(dataTree, sessions);
+    System.out.println(dataTree.getNodeCount());
   }
 
+  private static SnapshotExpiry parseCommandLine(String[] args) {
+    CommandLineParser parser = new DefaultParser();
+    Options options = new Options();
+
+    options.addRequiredOption(null, "snapshot-file", true, "Snapshot file location. (required)");
+    options.addRequiredOption(null, "expiry-days", true, "Znode expiry in days. (required)");
+    options.addRequiredOption(null, "server", true, "ZooKeeper server to connect. (required)");
+
+    options.addOption("h", "help", false, "Print help message");
+    options.addOption("c", "ctime", false, "Use ctime to calculate expiration. (default: mtime)");
+    options.addOption("n", "dry-run", false, "Don't delete the znodes, just list them.");
+
+    try {
+      CommandLine cli = parser.parse(options, args);
+      if (cli.hasOption("help")) {
+        printHelpAndExit(0, options);
+      }
+
+      return new SnapshotExpiry(cli.getOptionValue("snapshot-file"));
+    } catch (ParseException e) {
+      System.out.printf("%s\n\n", e.getMessage());
+      printHelpAndExit(1, options);
+      return null;
+    }
+  }
+
+  private static void printHelpAndExit(int exitCode, Options options) {
+    HelpFormatter help = new HelpFormatter();
+    help.printHelp("SnapshotExpiry", options, true);
+    System.exit(exitCode);
+  }
+
+  @Override
+  public void close() throws Exception {
+    // empty
+  }
 }
